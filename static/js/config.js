2cafbc64b81ab5cfa0b0469ccaea2dd8537eaeb1
pleyba <68707769+pleyba@users.noreply.github.com>
2020-10-22 14:51:02 -0500

    Update config.js

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,2 @@
 //API key
-const API_KEY = pk.eyJ1IjoicGxleWJhIiwiYSI6ImNrZ2w2aWk2ajBkMjgydnRub3M0dXRtZ3MifQ.w0B6gvrt5CDelNlTOXwgyg
+const API_KEY = "pk.eyJ1IjoicGxleWJhIiwiYSI6ImNrZ2w2aWk2ajBkMjgydnRub3M0dXRtZ3MifQ.w0B6gvrt5CDelNlTOXwgyg";
