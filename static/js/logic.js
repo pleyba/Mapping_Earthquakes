a0d0d3d33ff84120bcfeea2fcc94a2be3525cbea
pleyba <68707769+pleyba@users.noreply.github.com>
2020-10-28 14:36:30 -0500

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -32,14 +32,26 @@ let map = L.map('mapid', {
 L.control.layers(baseMaps).addTo(map);
 
 // Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+light.addTo(map);
 
 // Accessing the airport GeoJSON URL
-let torontoData = "https://raw.githubusercontent.com/pleyba/Mapping_Earthquakes/Mapping_GeoJSON_Points/torontoRoutes.json";
+let torontoData = "https://raw.githubusercontent.com/pleyba/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/torontoRoutes.json";
+
+// Create a style for the lines.
+let myStyle = {
+    color: "#ffffa1",
+    weight: 2
+}
 
 // Grabbing our GeoJSON data.
 d3.json(torontoData).then(function(data) {
     console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data).addTo(map);
+  L.geoJson(data, {
+      style: myStyle,
+      onEachFeature: function(feature, layer) {
+          layer.bindPopup("<h3> Airline: " + feature.properties.airline + "<h3> <hr><h3> Destination: "
+          + feature.properties.dst + "</h3>");
+      }
+  }).addTo(map);
 });
